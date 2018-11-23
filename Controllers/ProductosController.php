9ecc25472de480b1eb59e8e595ae9dcc39358878
jose ricardo <jose.cohuocaamal@itsva.edu.mmx>
2018-11-23 11:51:20 -0600

    taller software-CreacionControlesProductos

---
+++ Controllers/ProductosController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+if(isset($_POST['buttonCreate'])){
+    
+}
+elseif(isset ($_POST['buttonRead'])){
+    
+}
+elseif(isset ($_POST['buttonUpdate'])){
+    
+}
+elseif (isset ($_POST['buttonDelate'])) {
+
+}
+
+
+?>
