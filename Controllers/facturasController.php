590e9e728f136af98a6ed5850b09bb830c640f34
Daniel Øy Mis <ric-daniel-lpu12@hotmail.com>
2018-11-23 12:46:50 -0600

    taller-Software - Creacion de las funciones principales del CRUD, definicion aun no tiene codigo que conecte con la bd

--- Controllers/facturasController.php
+++ Controllers/facturasController.php
@@ -1,56 +1,39 @@
 <?php
 
-
 if (isset($_POST['buttonCreate'])) {
     //llamar a crear
-    
 } else if (isset($_POST['buttonRead'])) {
     
 } else if (isset($_POST['buttonUpdate'])) {
     
 } else if (isset($_POST['buttonDelete'])) {
     
-}  else {   //en caso de ejecutarse alguna de las opciones anteriores, se regresa al menu principal
+} else {   //en caso de ejecutarse alguna de las opciones anteriores, se regresa al menu principal
     header("Location: ../meunu/");
 }
 
-class FacturasController{
+class FacturasController {
+
     //funcion que realiza las acciones de inserccion de registros a la tabla de facturas
-    function create(){
+    function create() {
         
     }
-    
+
     //funcion de consulta a la tabla de facturas
-    function read(){
+    function read() {
         
     }
-    
+
     //funcion que actualiza un determinado registro
-    function update(){
+    function update() {
         
     }
-    
+
     //funcion que elimina registros de la tabla 
-    function delete(){
+    function delete() {
         
     }
-    
+
 }
-else{
-    header("Location: ../menu/");
-}
-class ComisionController{
-    function create(){
-        
-    }
-    function read(){
-        
-    }
-    function Update(){
-        
-    }
-    function Delete(){
-        
-    }
-}
+
 ?>
