26cb7bbfac88666c543974fd2e0e05ec21db8988
Daniel Øy Mis <ric-daniel-lpu12@hotmail.com>
2018-11-23 12:27:30 -0600

    taller-Software - Creacion de las funciones principales del CRUD, definicion aun no tiene codigo que conecte con la bd

--- Controllers/facturasController.php
+++ Controllers/facturasController.php
@@ -1,10 +1,6 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 if (isset($_POST['buttonCreate'])) {
     
 } else if (isset($_POST['buttonRead'])) {
@@ -12,6 +8,31 @@ if (isset($_POST['buttonCreate'])) {
 } else if (isset($_POST['buttonUpdate'])) {
     
 } else if (isset($_POST['buttonDelete'])) {
+    
+}  else {   //en caso de ejecutarse alguna de las opciones anteriores, se regresa al menu principal
+    header("Location: ../meunu/");
+}
+
+class FacturasController{
+    //funcion que realiza las acciones de inserccion de registros a la tabla de facturas
+    function create(){
+        
+    }
+    
+    //funcion de consulta a la tabla de facturas
+    function read(){
+        
+    }
+    
+    //funcion que actualiza un determinado registro
+    function update(){
+        
+    }
+    
+    //funcion que elimina registros de la tabla 
+    function delete(){
+        
+    }
     
 }
 ?>
