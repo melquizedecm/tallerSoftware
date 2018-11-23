eea2614cd2c54d482fcdfac14ad801a8e843379d
Daniel Øy Mis <ric-daniel-lpu12@hotmail.com>
2018-11-23 12:55:23 -0600

    taller-Software - Creacion de Modelo para facturas

---
+++ Models/Facturas.php
@@ -0,0 +1,32 @@
+<?php
+
+class Factura{
+    var $id;
+    var $id_cliente;
+    var $id_comision;
+    var $fecha;
+    var $estado;
+    var $total;
+    
+    function __construct($id, $id_cliente, $id_comision, $fecha, $estado, $total) {
+        $this->id = $id;
+        $this->id_cliente = $id_cliente;
+        $this->id_comision = $id_comision;
+        $this->fecha = $fecha;
+        $this->estado = $estado;
+        $this->total = $total;
+    }
+    
+    function create(){
+        $sql = "INSERT INTO factura(id_cliente,id_comision,fecha,estado,total) VALUES (".$this->id_cliente.",".$this->id_comision.",'".$this->fecha."','".$this->estado."',".$this->total.")";
+        $response = getResultSQL($sql);
+        if (!$response) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    
+    
+}
