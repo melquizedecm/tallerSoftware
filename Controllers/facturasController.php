606a3b3ff5b094c89c312a08ea0d9425b7e7412a
Daniel Øy Mis <ric-daniel-lpu12@hotmail.com>
2018-11-23 11:50:20 -0600

    taller-Software - Creacion de archivo del controlador de facturas

---
+++ Controllers/facturasController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+if (isset($_POST['buttonCreate'])) {
+    
+} else if (isset($_POST['buttonRead'])) {
+    
+} else if (isset($_POST['buttonUpdate'])) {
+    
+} else if (isset($_POST['buttonDelete'])) {
+    
+}
+?>
