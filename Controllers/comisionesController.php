4b58e21c3e62050558fa08b152df11956255f538
antonio alberto <antonio alberto@DESKTOP-A6LRKO3>
2018-11-23 11:57:45 -0600

    antonio

---
+++ Controllers/comisionesController.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+if (isset($_POST['buttonCreate'])){
+    
+}elseif (isset($_POST['buttonRead'])){
+    
+}elseif (isset($_POST['buttonUpdate'])){
+    
+}elseif (isset($_POST['buttonDelete'])){
+    
+}
