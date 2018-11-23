a463a31e69b29a8957a0d78787e454145c8b365e
“Melqui” <melqui21@hotmail.com>
2018-11-23 12:47:20 -0600

    tallerSoftware - modelos actualizados

--- Controllers/controller.php
+++ Controllers/controller.php
@@ -5,4 +5,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+if(isset($_POST['buttonCreate'])){
+    
+}elseif(isset($_POST['buttonRead'])){
+    //
+}elseif(isset($_POST['buttonUpdate'])){
+    
+}elseif(isset($_POST['buttonDelete'])){
+    
+}
+else{
+    header("Location: ../menu/");
+}
 
+class ComisionController{
+    
+    function create(){
+        
+    }
+    function read(){
+        
+    }
+    
+}
