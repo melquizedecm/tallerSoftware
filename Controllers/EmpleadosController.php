7407e9ea5228d991dcbab2a445016d64d16ca0ff
Emmanuel Ay <emmanuel.aytuz@itsva.edu.mx>
2018-11-23 14:54:56 -0600

    empleados - actualización

--- Controllers/EmpleadosController.php
+++ Controllers/EmpleadosController.php
@@ -1,17 +1,51 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-if (isset($_POST['buttonCreate'])){
+if (isset($_POST['buttonCreate'])) {
+    require_once '../lib/links.php';
+    libnivel2();
+    $empleadosController = new EmpleadosController();
+    $empleadosController->create();
+} elseif (isset($_POST['buttonRead'])) {
     
-}elseif (isset($_POST['buttonRead'])){
+} elseif (isset($_POST['buttonUpdate'])) {
     
-}elseif (isset($_POST['buttonUpdate'])){
+} elseif (isset($_POST['buttonDelete'])) {
     
-}elseif (isset($_POST['buttonDelete'])){
-    
+} else {
+    header("location:../menu/");
 }
+
+class PerfilesControler {
+
+    function create() {
+        ///1. recibir variables
+        $id = $_POST['inputId'];
+        $nombre = $_POST['inputNombre'];
+        $status = $_POST['inputStatus'];
+        //2. llamar al Modelo
+        require_once '.../Models/empleados.php';
+        $empleado = new empleados($id,$nombre,$status);
+        $empleado->create();
+    }
+
+    function read() {
+        
+    }
+
+    function update() {
+        
+    }
+
+    function delete() {
+        
+    }
+
+}
+
+?>
