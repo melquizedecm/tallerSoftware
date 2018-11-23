e4902a79d95b673e9f7fd780849b5178dc13348d
jose ricardo <jose.cohuocaamal@itsva.edu.mmx>
2018-11-23 11:51:36 -0600

    taller software-CreacionModelo

--- Models/model.php
+++ Models/model.php
@@ -5,4 +5,92 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+class productos {
 
+    var $id;
+    var $username;
+    var $categoria_id;
+    var $nombre;
+    var $cantidad;
+    var $valor_unitario;
+    var $estado;
+    var $usuario_id;
+    function __construct($id, $username, $categoria_id, $nombre, $cantidad, $valor_unitario, $estado, $usuario_id) {
+        $this->id = $id;
+        $this->username = $username;
+        $this->categoria_id = $categoria_id;
+        $this->nombre = $nombre;
+        $this->cantidad = $cantidad;
+        $this->valor_unitario = $valor_unitario;
+        $this->estado = $estado;
+        $this->usuario_id = $usuario_id;
+    }
+    function getId() {
+        return $this->id;
+    }
+
+    function getUsername() {
+        return $this->username;
+    }
+
+    function getCategoria_id() {
+        return $this->categoria_id;
+    }
+
+    function getNombre() {
+        return $this->nombre;
+    }
+
+    function getCantidad() {
+        return $this->cantidad;
+    }
+
+    function getValor_unitario() {
+        return $this->valor_unitario;
+    }
+
+    function getEstado() {
+        return $this->estado;
+    }
+
+    function getUsuario_id() {
+        return $this->usuario_id;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setUsername($username) {
+        $this->username = $username;
+    }
+
+    function setCategoria_id($categoria_id) {
+        $this->categoria_id = $categoria_id;
+    }
+
+    function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    function setCantidad($cantidad) {
+        $this->cantidad = $cantidad;
+    }
+
+    function setValor_unitario($valor_unitario) {
+        $this->valor_unitario = $valor_unitario;
+    }
+
+    function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+    function setUsuario_id($usuario_id) {
+        $this->usuario_id = $usuario_id;
+    }
+
+}
+
+
+
+?>
