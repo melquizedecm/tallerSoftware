d3486f3f147ed2d20455f4f93d2b10150087ac1e
“Melqui” <melqui21@hotmail.com>
2018-11-24 12:32:26 -0600

    Perfiles Modelo-Controller

--- Views/perfiles/index.php
+++ Views/perfiles/index.php
@@ -1,4 +1,6 @@
 <?php
+require_once '../../lib/links.php';
+libnivel3();
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +10,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <?php        getMeta("Perfiles");?>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
